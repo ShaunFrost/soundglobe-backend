6864f193003587f530fb96ba11d3a696d102a8f2
rasesh <raseshrout@gmail.com>
2024-07-03 01:06:52 +0530

    add imagekit as storage for creating dynamic page

--- server.js
+++ server.js
@@ -3,20 +3,36 @@ const bodyParser = require('body-parser')
 const app = express()
 const port = 8080
 const cors = require('cors')
-const { createSHA256Hash } = require('./utils')
+const dotenv = require('dotenv')
+const { createSHA256Hash, getImageBuffer } = require('./utils')
+const ImageKit = require("imagekit")
 
-const generatedImages = {abc: 'abc'}
+require('dotenv').config()
+
+const imagekit = new ImageKit({
+    publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
+    privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
+    urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT
+});
+
+const generatedImages = {}
 
 app.use(bodyParser.json())
 app.use(cors())
 
-app.post('/create', (req, res) => {
+app.post('/create', async (req, res) => {
     const { email, imageData } = req.body
     const imageId = createSHA256Hash(email)
-    generatedImages[imageId] = imageData
+    const imageBuffer = getImageBuffer(imageData)
+    const resp = await imagekit.upload({
+        file: imageBuffer,
+        fileName: imageId + '.jpeg',
+        useUniqueFileName: false
+    })
+    generatedImages[imageId] = resp.url
     res.json({
         success: true,
-        id: imageId
+        id: imageId,
     })
 })
 
@@ -26,9 +42,29 @@ app.get('/', (req, res) => {
 
 app.get('/image/:imageId', (req, res) => {
     const { imageId } = req.params;
-    const data = generatedImages[imageId];
-    if (data) {
-        res.json({imageData: data})
+    const imageUrl = generatedImages[imageId];
+    if (imageUrl) {
+        const thumbnailUrl = imagekit.url({
+            src: imageUrl,
+            transformation: [{
+                width: 200,
+                height: 200,
+                quality: 75
+            }]
+        });
+        res.send(`
+            <html>
+                <head>
+                    <meta name="twitter:card" content="summary">
+                    <meta name="twitter:title" content="My favorite artist country">
+                    <meta name="twitter:description" content="Map representation of the country of my most listened artists">
+                    <meta name="twitter:image" content="${thumbnailUrl}">
+                </head>
+                <body>
+                    <img src="${imageUrl}" />
+                </body>
+            </html>
+        `)
     } else {
         res.status(404).send('Image not found');
     }
--- utils.js
+++ utils.js
@@ -1,10 +1,18 @@
 const crypto = require('crypto')
+
 function createSHA256Hash(inputString) {
     const hash = crypto.createHash('sha256');
     hash.update(inputString);
     return hash.digest('hex');
 }
 
+function getImageBuffer(imageData) {
+    const commaIndex = imageData.indexOf(',')
+    const base64Data = imageData.substr(commaIndex + 1)
+    return Buffer.from(base64Data, 'base64')
+}
+
 module.exports = {
-    createSHA256Hash
+    createSHA256Hash,
+    getImageBuffer
 }
